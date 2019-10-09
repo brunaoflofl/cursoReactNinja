9cddbd53088ab207699ade1bd9c2c6dc956adb61
brunaoflofl <bruna.oflima@gmail.com>
2019-10-09 14:21:50 -0300

    reduce

--- jest-in-practice/src/filter.js
+++ jest-in-practice/src/filter.js
@@ -3,7 +3,7 @@ const filter = (arr, func) => {
     let newArr = []
 
     for(let i = 0; i < arr.length; i++) {
-        if(func(arr[i])){
+        if(func(arr[i])){ //verifica se o valor do item retorna true, se retornar o valor é passado para o arrey. (como o filter funciona)
             newArr.push(arr[i])
         }
     }
